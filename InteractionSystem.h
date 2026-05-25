deeff36caf2c2b4d3ebdd4a26a7bf1660b2b3e71
Felorex <np47z5vxbs@privaterelay.appleid.com>
2026-05-25 18:03:34 +0700

    Add Dog class interaction to InteractionSystem

--- InteractionSystem.h
+++ InteractionSystem.h
@@ -3,6 +3,7 @@
 
 #include "Player.h"
 #include "DynamicObject.h"
+#include "Dog.h"
 
 static bool equal(float a, float b, float epsilon = 1.f) {
 	return std::abs(a - b) < epsilon;
@@ -11,6 +12,6 @@ static bool equal(float a, float b, float epsilon = 1.f) {
 class InteractionSystem {
 public:
 	bool canInteract(Player& player, DynamicObject& object);
-	bool canRightInteract(Player& player, DynamicObject& object);
+	bool canBiting(Player& player, Dog& dog);
 };
 #endif // !INTRACTIONSYSTEM_H
