2e1d91657650a06e9e9fdecae4e8b4cce30f763c
Felorex <np47z5vxbs@privaterelay.appleid.com>
2026-05-25 17:59:49 +0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -10,11 +10,16 @@ Player::Player() {
 	creepHeight = size.height / 2;
 	rect.setSize(sf::Vector2f(size.width, size.height));
 	pos.x = 50.f;
-	pos.y = 500.f;
+	pos.y = 800.f;
+
 	isInteracting = false;
 	normalSpeed = 200.f;
+	dogsTerritory = 1380.f;
 }
-float Player::getNormalSpeed() {
+float Player::getDogsTerritory() {
+	return dogsTerritory;
+}
+float Player::getSpeed() {
 	return normalSpeed;
 }
 void Player::jump() {
@@ -35,6 +40,10 @@ void Player::drRect() {
 	rect.setPosition(pos.x, pos.y);
 	rect.setFillColor(sf::Color::Red);
 }
+void Player::setIsCreeping(bool creeping) {
+	isCreeping = creeping;
+	creep();
+}
 void Player::creep() {
 	if (isCreeping) {
 		setSize(size.width, creepHeight);
@@ -44,12 +53,6 @@ void Player::creep() {
 		setSize(size.width, normalHeight);
 		rect.setSize(sf::Vector2(size.width, normalHeight));
 	}
-}
-void Player::setIsCreeping(bool creep) {
-	isCreeping = creep;
-}
-bool Player::getIsCreeping() {
-	return isCreeping;
 }
 void Player::setIsInteracting(bool interact) {
 	isInteracting = interact;
